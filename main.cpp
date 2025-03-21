cf6d7b96e4499db3c3033a654460f72934aa5eb9
bar2011 <bareinav10@gmail.com>
2025-03-21 15:25:20 +0200

    Use Layer::Dense to run arbitrary image through one layer of neurons and print the result

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include "layer.h"
 #include "loader.h"
+#include <algorithm>
+#include <iostream>
 #include <string_view>
+#include <tuple>
 
 std::array<MNistLoader::DataPair, 2> loadData() {
   std::string_view trainingLabelsPath{"./data/train-labels-idx1-ubyte"};
@@ -15,7 +19,20 @@ std::array<MNistLoader::DataPair, 2> loadData() {
 }
 
 int main() {
-  auto loader{loadData()};
+  auto mnistData{loadData()};
+
+  // rows and columns in each image
+  constexpr int rows{28};
+  constexpr int cols{28};
+
+  auto testLayer{new Layer::Dense<rows * cols, 10>()};
+  testLayer->forward(std::get<1>(mnistData[0])[0].data().data());
+
+  std::get<1>(mnistData[0])[0].print();
+  std::cout << '\n';
+
+  std::cout << "guess: " << std::max_element(testLayer->getOutput().begin(), testLayer->getOutput().end()) - testLayer->getOutput().begin() << '\n';
+  std::cout << "real: " << static_cast<int>(std::get<0>(mnistData[0])[0]) << '\n';
 
   return 0;
 }
